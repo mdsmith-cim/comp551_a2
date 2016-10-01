f24020f38a5d7c5d3543105b18a4106ff5aab5af
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-10-01 10:23:25 -0400

    Update to preprocess

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ import numpy as np
 from util.preprocess import preprocess
 
 pp = preprocess()
-X_train, y_train, X_test = pp.get_data_process(max_features=5000)
+
+# Raw data
+#X_train, y_train, X_test = pp.get_data_raw()
+
+# No numbers, but otherwise identical to raw including string categories
+#X_train, y_train, X_test = pp.get_data_no_numbers()
+
+# With no numbers and stop words removed; basically just words with spaces in between
+#X_train, y_train, X_test = pp.get_data_no_stop_words()
+
+# All of the above + processed into bag of words
+# Output is a sparse matrix
+# Set the max_features argument if you want to change the limit at which it cuts off (default: 5000)
+# Bag of words will provide the MAX_FEATURES most common words
+# Even with 5000 features, one abstract might only have 70-100 non-zero features (words)
+# Execution time is ~25 sec on my laptop
+X_train, y_train, X_test = pp.get_bagofwords()
+
+
 
 # Do stuff with data...
 
--- util/preprocess.py
+++ util/preprocess.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 # If you dont' have this, follow this guide
 # https://spacy.io/docs/#getting-started
 #import spacy
 
 class preprocess:
 
-    def get_data_raw(self, train_in = 'data/train_in.csv', train_out = 'data/train_out.csv', test_in = 'data/test_in.csv'):
+    def __init__(self, train_in = 'data/train_in.csv', train_out = 'data/train_out.csv', test_in = 'data/test_in.csv' ):
 
-        train = pd. read_csv(train_in)
-        train_y = pd.read_csv(train_out)
-        test = pd.read_csv(test_in)
+        self.categories = ['math', 'cs', 'stat', 'physics']
+        self.uniques = None
+        self.train_in = train_in
+        self.train_out = train_out
+        self.test_in = test_in
+
+    def get_data_raw(self, ):
+
+        train = pd. read_csv(self.train_in)
+        train_y = pd.read_csv(self.train_out)
+        test = pd.read_csv(self.test_in)
 
         X_train = train['abstract'].values
         X_test = test['abstract'].values
@@ -19,7 +28,7 @@ class preprocess:
 
         return X_train, y_train, X_test
 
-    def get_data_process(self, max_features=5000):
+    def get_data_no_numbers(self):
 
         X_train, y_train, X_test = self.get_data_raw()
 
@@ -30,15 +39,60 @@ class preprocess:
         for i in range(0, X_test.shape[0]):
             X_test[i] = re.sub("[^a-zA-Z]", " ", X_test[i])
 
+        return X_train, y_train, X_test
+
+
+    def convert_string_category_to_num(self, y):
+
+        ser = pd.Series(y)
+
+        labels, uniques = pd.factorize(ser)
+
+        self.uniques = uniques
+
+        return labels
+
+    def convert_num_category_to_string(self, y):
+
+        if self.uniques is None:
+            raise Exception('Please convert strings to categories first')
+
+        return np.array(self.uniques[y])
+
+
+    def get_data_no_stop_words(self):
+
+        X_train, y_train, X_test = self.get_data_no_numbers()
+
+        # Use vectorizer here to convert to lower case, remove stop words
+        # We don't actually perform bag of words here
+        vectorizer = CountVectorizer(analyzer="word", strip_accents="unicode", stop_words="english")
+        analyser = vectorizer.build_analyzer()
+
+        # Remvoe stop words
+        for i in range(0, X_train.shape[0]):
+            X_train[i] = " ".join(analyser(X_train[i]))
+
+        for i in range(0, X_test.shape[0]):
+            X_test[i] = " ".join(analyser(X_test[i]))
+
         # TODO: Lots of NLP
         # nlp = spacy.load('en')
         # -validate effectiveness of removing stop words and other transforms like number removal
 
-        # Run bag of words, include basic stop words list, lowercase transform is default
-        vectorizer = CountVectorizer(analyzer= "word", strip_accents= "unicode", stop_words= "english", max_features=max_features )
+        return X_train, y_train, X_test
+
+    def get_bagofwords(self, max_features=5000):
+
+        X_train, y_train, X_test = self.get_data_no_numbers()
+
+        # Run bag of words, include basic stop words list, lower case transform
+        vectorizer = CountVectorizer(analyzer= "word", strip_accents= "unicode", stop_words= "english", max_features=max_features)
 
         X_train = vectorizer.fit_transform(X_train)
         X_test = vectorizer.fit_transform(X_test)
+
+        y_train = self.convert_string_category_to_num(y_train)
 
         return X_train, y_train, X_test
 
