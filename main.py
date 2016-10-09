270a21da656727f091259d9d5b8dbb86de150699
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-10-09 12:14:39 -0400

    Added NLP preprocessing

--- main.py
+++ main.py
@@ -19,9 +19,18 @@ pp = preprocess()
 # Bag of words will provide the MAX_FEATURES most common words
 # Even with 5000 features, one abstract might only have 70-100 non-zero features (words)
 # Execution time is ~25 sec on my laptop
-X_train, y_train, X_test = pp.get_bagofwords()
+#X_train, y_train, X_test = pp.get_bagofwords(max_features=5000, use_spacy=False)
+
+# If you want bag of words based on the root of words from spacy natural language processing
+X_train, y_train, X_test = pp.get_bagofwords(max_features=5000, use_spacy=True)
 
 
+# If you want data processed with spacy but not yet run through bag of words:
+#X_train, y_train, X_test = pp.get_data_nlp()
+
+# Remove pp object to save memory
+# having spacy in memory consumes ~1.7GB
+del(pp)
 
 # Do stuff with data...
 
--- util/preprocess.py
+++ util/preprocess.py
@@ -1,20 +1,28 @@
 import pandas as pd
-import re
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from nltk.corpus import stopwords
 import numpy as np
-# If you dont' have this, follow this guide
-# https://spacy.io/docs/#getting-started
-#import spacy
+import spacy
+import re
+import os
+
 
 class preprocess:
 
-    def __init__(self, train_in = 'data/train_in.csv', train_out = 'data/train_out.csv', test_in = 'data/test_in.csv' ):
+    def __init__(self, train_in = 'data/train_in.csv', train_out = 'data/train_out.csv', test_in = 'data/test_in.csv'):
 
         self.categories = ['math', 'cs', 'stat', 'physics']
         self.uniques = None
         self.train_in = train_in
         self.train_out = train_out
         self.test_in = test_in
+
+        # Below is for NLP processing only
+        self.STOP_WORDS_PLUS_EXTRA = frozenset(list(ENGLISH_STOP_WORDS) + stopwords.words("english") + [';', "'s"])
+        self.SYMBOL_REMOVE_LIST = frozenset(["(", ")", "^", ".", ",", "[", "]", "-", "--", "{", "}", "=", "/", ":",
+                                             "`", "|", "<", ">", "\\", '"', "'", "%", "*"])
+        self.nlp = spacy.load('en')
 
     def get_data_raw(self, ):
 
@@ -69,32 +77,170 @@ class preprocess:
         vectorizer = CountVectorizer(analyzer="word", strip_accents="unicode", stop_words="english")
         analyser = vectorizer.build_analyzer()
 
-        # Remvoe stop words
+        # Remove stop words
         for i in range(0, X_train.shape[0]):
             X_train[i] = " ".join(analyser(X_train[i]))
 
         for i in range(0, X_test.shape[0]):
             X_test[i] = " ".join(analyser(X_test[i]))
 
-        # TODO: Lots of NLP
-        # nlp = spacy.load('en')
-        # -validate effectiveness of removing stop words and other transforms like number removal
-
         return X_train, y_train, X_test
 
-    def get_bagofwords(self, max_features=5000):
+    def get_bagofwords(self, max_features=5000, data_directory='nlp_processed/', clean=False, use_disk=True, use_spacy=True):
 
-        X_train, y_train, X_test = self.get_data_no_numbers()
+        filename = os.path.join(data_directory, 'data_bow.dat')
 
-        # Run bag of words, include basic stop words list, lower case transform
-        vectorizer = CountVectorizer(analyzer= "word", strip_accents= "unicode", stop_words= "english", max_features=max_features)
+        if clean:
+            os.remove(filename)
+            print("Deleted existing data from disk")
+
+        if use_disk:
+            try:
+                file = open(filename, 'rb')
+                loaded = np.load(file)
+
+                if 'X_train' not in loaded.files or 'X_test' not in loaded.files or 'y_train' not in loaded.files:
+                    file.close()
+                    raise Exception('Saved data is corrupted')
+
+                X_train = loaded['X_train']
+                X_test = loaded['X_test']
+                y_train = loaded['y_train']
+
+                file.close()
+
+                print("Loaded data from file " + filename)
+
+                return X_train, y_train, X_test
+
+            except Exception as e:
+                print("Error loading saved data;" + str(e))
+
+                try:
+                    os.mkdir(data_directory)
+                # do nothing if it already exists
+                except FileExistsError:
+                    pass
+
+        if use_spacy:
+
+            X_train, y_train, X_test = self.get_data_raw()
+            # Bag of words: run on preprocessed data
+            vectorizer = CountVectorizer(analyzer="word", strip_accents="unicode", tokenizer=self.process_spacy_sample,
+                                         max_features=max_features)
+        else:
+            X_train, y_train, X_test = self.get_data_no_numbers()
+            # Bag of words: use scikit-learn built in stop words
+            vectorizer = CountVectorizer(analyzer="word", strip_accents="unicode", stop_words="english",
+                                         max_features=max_features)
 
         X_train = vectorizer.fit_transform(X_train)
         X_test = vectorizer.fit_transform(X_test)
 
         y_train = self.convert_string_category_to_num(y_train)
 
+        if use_disk:
+            file = open(filename, 'wb')
+            np.savez_compressed(file, X_train=X_train, X_test=X_test, y_train=y_train)
+            print("Saved data to " + filename)
+            file.close()
+
         return X_train, y_train, X_test
 
+    def get_data_nlp(self, use_disk=True, data_directory='nlp_processed/', clean=False):
 
+        filename = os.path.join(data_directory, 'data_nlp.dat')
+
+        if clean:
+            os.remove(filename)
+            print("Deleted existing data from disk")
+
+        if use_disk:
+
+            try:
+                file = open(filename, 'rb')
+                loaded = np.load(file)
+
+                if 'X_train' not in loaded.files or 'X_test' not in loaded.files or 'y_train' not in loaded.files:
+                    file.close()
+                    raise Exception('Saved data is corrupted')
+
+                X_train = loaded['X_train']
+                X_test = loaded['X_test']
+                y_train = loaded['y_train']
+
+                file.close()
+
+                print("Loaded data from file " + filename)
+
+                return X_train, y_train, X_test
+
+            except Exception as e:
+                print("Error loading saved data;" + str(e))
+
+                try:
+                    os.mkdir(data_directory)
+                # do nothing if it already exists
+                except FileExistsError:
+                    pass
+
+        X_train, y_train, X_test = self.get_data_raw()
+
+        # If you dont' have this, follow this guide
+        # https://spacy.io/docs/#getting-started
+
+        X_train = np.array(self.process_spacy(X_train))
+        X_test = np.array(self.process_spacy(X_test))
+
+        y_train = self.convert_string_category_to_num(y_train)
+
+        if use_disk:
+            file = open(filename, 'wb')
+            np.savez_compressed(file, X_train=X_train, X_test=X_test, y_train=y_train)
+            print("Saved data to " + filename)
+            file.close()
+
+        return X_train, y_train, X_test
+
+    def process_spacy(self, data):
+
+        results = []
+
+        for i in range(0, data.shape[0]):
+
+            results.append(" ".join(self.process_spacy_sample(data[i])))
+
+        return results
+
+    def process_spacy_sample(self, text):
+
+        #  We need to do a little preprocessing to remove some symbols otherwise spacy does strange things when
+        # it parses
+
+        # Remove any newlines just in case (don't think there are any in the data but just in case)
+        text = text.strip()
+
+        # Strip latex - cheap method: remove everything between $'s
+        text = re.sub('\$[^$]+\$', '', text)
+
+        for rm in self.SYMBOL_REMOVE_LIST:
+            text = text.replace(rm, " ")
+
+        lemmas = []
+        parsed = self.nlp(text)
+        for token in parsed:
+            # token has lemma unless it is a pronoun, in which case it is assigned the string -PRON-
+            # so in that case we simply use the word itself
+            # See https://spacy.io/docs/#token
+            if not token.is_digit:
+                lemmas.append(token.lemma_.lower().strip() if token.lemma_ != "-PRON-" else token.lower_)
+
+        parsed = lemmas
+
+        # Remove all stop words and other undesirables
+        # also make sure not an empty string
+        parsed = [token for token in parsed if
+                  (token not in self.STOP_WORDS_PLUS_EXTRA and len(token) != 0)]
+
+        return parsed
 
