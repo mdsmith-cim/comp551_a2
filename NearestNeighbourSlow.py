4f5b54e3d696701c91bd617e589ade81467a2e6d
rmc17 <rebecca.carrington@mail.mcgill.ca>
2016-10-13 20:29:28 -0400

    for Michael to run

--- NearestNeighbourSlow.py
+++ NearestNeighbourSlow.py
@@ -1,15 +1,18 @@
 import numpy as np
-from numpy import linalg as LA
+#from numpy import linalg as LA
 import random
 from util.preprocess import preprocess
-from scipy import sparse
-import time
+#from scipy import sparse
+#import time
 from scipy.sparse.linalg import norm
 import math
 from scipy.sparse import csr_matrix
+from statistics import mode, StatisticsError, stdev, mean
+#import random
+#from sklearn.model_selection import train_test_split
 
 
-
+# not used for cross validation
 # separate training data into training and validation randomly
 def data_separation(num_tot_train, ratio):
     
@@ -27,7 +30,7 @@ def data_separation(num_tot_train, ratio):
 
 
 # finds the accuracy
-def evaluate(predictions, test_out):
+def evaluate(predictions, test_out, descr):
     tot = 0
     n = 0
     
@@ -37,60 +40,103 @@ def evaluate(predictions, test_out):
             tot += 1
         else:
             tot += 1
-    print('prediction success', n, tot, n/tot)
+    print(descr, 'prediction success', n, tot, n/tot)
+    accuracy = n/tot
+    return accuracy
 
 
-
-def get_predictions(X_train, x_test, train_ind, valid_ind):
+# returns the prediction based on a majority rule
+def pick_index(k, thelist, index_list, metric, y_train):
+# k is for kNN
+# thelist is not important for majority rule but for picking min, yes
+# index_list is the list of indices which are closest to the validation example
     
-    # pick metric
-    dotprod = 1 # not really a metric but works better
-    euclid = 1
+    class_list = []
+    for i in range(k):
+        class_list.append(y_train[index_list[i]])
+    
+    try: # there is a unique mode
+        md = mode(class_list)
+#        print('mode',md)
+        pred = md
+    except StatisticsError: # mode not unique
+        
+#        print("There is no mode!")
+        
+        # pick max in thelist
+        if(metric == 'dot'):
+            max_dot = max(thelist)
+            max_dot_index = thelist.index(max_dot)
+            index = index_list[max_dot_index]
+            pred = y_train[index]
+        
+        if(metric == 'euc'):
+            min_euc = min(thelist)
+            min_euc_index = thelist.index(min_euc)
+            index = index_list[min_euc_index]
+            pred = y_train[index]
+            
+    return pred
+
+
+
+''' COULD TRY DOT PRODUCT AGAIN '''
+
+
+
+def get_predictions(X_train, x_test, train_ind, valid_ind, k, y_train):
+    
+    X_copy = X_train
+    X_train = X_train[train_ind]
+    y_train = y_train[train_ind]
     
     diff = []
-    y_pred_dot = dict()
-    y_pred_euc = dict()
+    y_pred = dict()
     
     count = 1    
        
     for v_ind in valid_ind:
         
         # validation example is from training set, index v_ind
-        test = X_train[v_ind]
+        test = X_copy[v_ind]
+        diff = csr_matrix(X_train.toarray() - test)
+        norms = norm(diff,axis=1) # find norm of each row
         
-        dot = 0
-        diff = norm(test - X_train[train_ind[0]])
-        index_dot = 0
-        index_euc = 0
+        # norms should no longer be sparse. make list out of it
+        norms = norms.tolist()
+
+        # will make changes to this one and want to keep copy of norms
+        temp_norms = norms
         
-        # compare test to all examples in training set
-        for t_ind in train_ind:
-            
-            if dotprod == 1:
-                new_dot = test.dot(X_train[t_ind].T).toarray()
+        
+        # kNN
+        
+        k_neigh = []
+        neigh_norms = []
+        
+        for i in range(k):
 
-                if(new_dot > dot):
-                    index_dot = t_ind
-                    dot = new_dot                
-                
-                
-            elif euclid == 1:
-                new_diff = norm(test - X_train[t_ind])
-
-                if(new_diff < diff):
-                    index_euc = t_ind
-                    diff = new_diff
-            
-        y_pred_dot[v_ind] = y_train[index_dot]
-        y_pred_euc[v_ind] = y_train[index_euc]        
+            # find min norm
+            min_euc = min(temp_norms)
+            # add to list
+            neigh_norms.append(min_euc)
+            # find index of min norm
+            min_euc_index = temp_norms.index(min_euc)
+            # add to list
+            k_neigh.append(min_euc_index)
+            # remove from temp_norms so get new min next time if k>1
+            del temp_norms[min_euc_index]
+        
+        # get prediction
+        pred = pick_index(k, neigh_norms, k_neigh, 'euc', y_train)
+        
+        y_pred[v_ind] = pred
         
         count += 1
-        print(count)
         
-        if count == 10:
-            break    
-
-    return y_pred_dot, y_pred_euc
+#        if count == 10022:
+#            break
+    return y_pred
 
 
 
@@ -100,48 +146,135 @@ def get_predictions(X_train, x_test, train_ind, valid_ind):
 
 ''' ACTUALLY RUNNING THINGS STARTING HERE '''
 
+k = 1
+
+
 # Set path to data here if desired
 pp = preprocess()
 
 # X_train is with features as words
 # y_train is classification of X_train
 # X_test is test data with features as words
-#X_train, y_train, X_test = pp.get_bagofwords() #changing max features does not speed things up much.
-X_train, y_train, X_test = pp.get_tf_idf(max_features=10000, use_spacy=True)
+X_train, y_train, X_test = pp.get_tf_idf(max_features=5000, use_spacy=True)
 
-
+#X_train = X_train[0:1000]
+#y_train = y_train[0:1000]
 
 # total number of training examples
 num_tot_train = X_train.shape[0]
 # ratio of training examples to be used for validation
-valid_ratio = 1/10
+#valid_ratio = 1/4
 
 # separate training set into actual training and validation
-train_ind, valid_ind = data_separation(num_tot_train,valid_ratio)
-num_train = len(train_ind)
-num_valid = len(valid_ind)
+#train_ind, valid_ind = data_separation(num_tot_train,valid_ratio)
 
 
 # get the predictions
-y_pred_dot, y_pred_euc = get_predictions(X_train, X_train, train_ind, valid_ind)
+#y_pred = get_predictions(X_train, X_train, train_ind, valid_ind, k, y_train)
 
 # print out accuracy
-evaluate(y_pred_dot, y_train)
-evaluate(y_pred_euc, y_train)
+#accuracy = evaluate(y_pred, y_train,'precrossval')
+
+#all training examples and 100 features
+#prediction success 7598 8864 0.8571750902527075
+
+    
+    
+def crossval_sep(num_tot_train, ratio):
+    # size of validation set
+    size = math.ceil(ratio*num_tot_train)
+    
+    # all data
+    all_indices = list(range(0,num_tot_train))
+    random.shuffle(all_indices)
+    
+    # split into three lists
+    list1 = all_indices[0:size]
+    list2 = all_indices[size:2*size]
+    list3 = all_indices[2*size:3*size]
+    list4 = all_indices[3*size:]
+    
+    # three folds of cross validation
+    train_ind1 = list1+list2+list3
+    valid_ind1 = list4
    
+    train_ind2 = list1+list2+list4
+    valid_ind2 = list3
+    
+    train_ind3 = list1+list3+list4
+    valid_ind3 = list2
+    
+    train_ind4 = list2+list3+list4
+    valid_ind4 = list1
+    
+    return train_ind1, valid_ind1, train_ind2, valid_ind2, train_ind3, valid_ind3, train_ind4, valid_ind4
 
 
-'''
-cross validation to pick the best k
 
-problems
-too many zeros. picks abstract with least number of words.
-maybe to combat this take bunch of nearest neighbours.. but then will always take shortest ones?
+''' CROSS VALIDATION BEGINS '''
 
-'''
-'''
-TO DO
-deloop get_predictions
-do some cross validation
+k_options = [1,2,3,4,8,16,32]
+#k_options = []
+valid_ratio = 1/4
 
-'''
+train_ind1, valid_ind1, train_ind2, valid_ind2, train_ind3, valid_ind3, train_ind4, valid_ind4 = crossval_sep(num_tot_train,valid_ratio)
+
+
+
+''' TRAINING ERROR '''
+train_ind = range(num_tot_train)
+#print(train_ind)
+valid_ind = range(num_tot_train)
+#print(valid_ind)
+
+
+# loop over k after finding training and validation sets.
+
+# for each k option
+#    for each fold of cross validation
+#        validate on kth subset.. find error.
+#    find average prediction error over k folds
+# choose k with lowest average prediction error. or equivalently, highest accuracy.
+
+avg_acc = dict()
+stdev_acc = dict()
+
+train_acc = dict()
+
+for k in k_options:
+    print('k=',k)
+    
+    # validation
+    y_pred1 = get_predictions(X_train, X_train, train_ind1, valid_ind1, k, y_train)
+    acc1 = evaluate(y_pred1, y_train, 'fold1')
+    
+    y_pred2 = get_predictions(X_train, X_train, train_ind2, valid_ind2, k, y_train)
+    acc2 = evaluate(y_pred2, y_train, 'fold2')
+    
+    y_pred3 = get_predictions(X_train, X_train, train_ind3, valid_ind3, k, y_train)
+    acc3 = evaluate(y_pred3, y_train, 'fold3')
+    
+    y_pred4 = get_predictions(X_train, X_train, train_ind4, valid_ind4, k, y_train)
+    acc4 = evaluate(y_pred4, y_train, 'fold4')
+    
+    avg_acc[k] = mean([acc1,acc2,acc3,acc4])
+    stdev_acc[k] = stdev([acc1,acc2,acc3,acc4])    
+    
+    # training
+    y_pred_train = get_predictions(X_train, X_train, train_ind, valid_ind, k, y_train)
+    train_acc[k] = evaluate(y_pred_train, y_train, 'training')
+    
+
+print('')
+print('average accuracy for each k',avg_acc)
+print('stdev accuracy for each k',stdev_acc)
+print('training accuracy',train_acc)
+
+#optimal_k = max(avg_acc, key=avg_acc.get)
+
+print('\n')
+
+
+
+
+
