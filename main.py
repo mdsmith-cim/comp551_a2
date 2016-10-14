a9cc4a715d6f884f68f201da2d099d184da81b66
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-10-14 00:03:10 -0400

    Report preprocess code

--- main.py
+++ main.py
@@ -9,35 +9,6 @@ from sklearn.svm import SVC
 # Set path to data here if desired
 pp = preprocess()
 
-# Raw data
-#X_train, y_train, X_test = pp.get_data_raw()
-
-# No numbers, but otherwise identical to raw including string categories
-#X_train, y_train, X_test = pp.get_data_no_numbers()
-
-# With no numbers and stop words removed; basically just words with spaces in between
-#X_train, y_train, X_test = pp.get_data_no_stop_words()
-
-# All of the above + processed into bag of words
-# Output is a sparse matrix
-# Set the max_features argument if you want to change the limit at which it cuts off (default: 5000)
-# Bag of words will provide the MAX_FEATURES most common words
-# Even with 5000 features, one abstract might only have 70-100 non-zero features (words)
-# Execution time is ~25 sec on my laptop
-#X_train, y_train, X_test = pp.get_bagofwords(max_features=5000, use_spacy=False)
-
-# If you want bag of words based on the root of words from spacy natural language processing
-#X_train, y_train, X_test = pp.get_bagofwords(max_features=5000, use_spacy=True)
-
-
-# If you want data processed with spacy but not yet run through bag of words:
-#X_train, y_train, X_test = pp.get_data_nlp()
-
-# tf-idf is available
-# Note: all arguments are passed directly to get_bagofwords
-#X_train, y_train, X_test = pp.get_tf_idf(max_features=10000, use_spacy=True)
-
-
 X_train, y_train, X_test = pp.get_tf_idf(max_features=20000, use_spacy=True, ngram_range=(1, 2))
 
 # Remove pp object to save memory
@@ -54,7 +25,7 @@ classifierLin = grid_search_cv(X_train, y_train, parameters={'C': [0.01, 0.1, 1,
                                                              'dual': [True, False]},
                                classifier=LinearSVC(verbose=10, max_iter=100000), error_score=0)
 
-# Best parameters:
+# Best parameters Linear:
 # {'C': 1,
 #  'class_weight': None,
 #  'dual': True,
